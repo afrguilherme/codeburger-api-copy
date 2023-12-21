5bae233909a59447765eba906cb0e574ffd5d4ce
afrguilherme <augustogafr@gmail.com>
2023-12-21 12:48:28 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dialect: "postgres",
+  url: "postgresql://postgres:DGF1*12gf16c12**ddGddcfDB4A11D6g@monorail.proxy.rlwy.net:37033/railway",
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
